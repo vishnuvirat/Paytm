327cbd713ffdf62c34534d06d9388cc0eec1dafa
Vishnu <vishnuviratkohli5@gmail.com>
2021-11-04 02:43:29 +0530

    First commit

---
+++ src/components/Paytm.js
@@ -0,0 +1,10 @@
+export const Paytm = () => {
+  return (
+    <div>
+      <div className="flex justify-center items-center .rounded-lg border-black border-2">
+          <div><div className="rounded-full border-purple-400 border-2  p-6 text-white bg-purple-400">BM</div>
+          <div className="font-bold">Babban Mandal</div></div>
+      </div>
+    </div>
+  );
+};
